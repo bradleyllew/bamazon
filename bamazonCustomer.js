d2d406c32e444bb8a6ea4591ac0032f40294845c
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-10-02 12:31:06 -0400

    Initialized .js file and .sql file

---
+++ bamazonCustomer.js
@@ -0,0 +1,118 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var products = ["Kitty Condo $125", "Catnip Bag(sm) $5", "Catnip Bag(lg) $10", "Catnip Mouse $10", "Cat Bed $40", "Litterbox $7", "Kitty Litter $35", "Cosmic Toy $5", "Jingle Ball $25", "Crinkle Toy $25"];
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+// to display all the products available to customer, including id, name, & price
+function start() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query("SELECT item_id, product_name, price FROM products", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+    buy();
+}
+
+// asks customer the id of the product wanted and how many of that item
+function buy() {
+    inquirer
+        .prompt(
+            {
+                name: "username",
+                type: "input",
+                message: "What is your kitty's name?"
+            },
+            {
+                name: "toBuy",
+                type: "rawlist",
+                message: "What would you like to buy for your kitty?",
+                choices: ["Kitty Condo $125", "Catnip Bag(sm) $5", "Catnip Bag(lg) $10", "Catnip Mouse $10", "Cat Bed $40", "Litterbox $7", "Kitty Litter $35", "Cosmic Toy $5", "Jingle Ball $25", "Crinkle Toy $25"]
+            },
+            {
+                name: "howMany",
+                type: "number",
+                message: "How many would you like?"
+            })
+        .then(function (answer) {
+            // based on their answer, welcome owner & kitty
+            if (inquirerResponse.username) {
+                console.log(inquirerResponse.username + " is going to love their new " + inquirerResponse.choices);
+              }
+            // based on their answer, check the stock quantities
+            switch (answer) {
+                case "Kitty Condo $125":
+                  checker();
+                  break;
+                
+                case "Catnip Bag(sm) $5":
+                  checker();
+                  break;
+                
+                case "Catnip Bag(lg) $10":
+                  checker();
+                  break;
+                
+                case "Catnip Mouse $10":
+                  checker();
+                  break;
+
+                  case "Cat Bed $40":
+                  checker();
+                  break;
+
+                  case "Litterbox $7":
+                  checker();
+                  break;
+
+                  case "Kitty Litter $35":
+                  checker();
+                  break;
+
+                  case "Cosmic Toy $5":
+                  checker();
+                  break;
+
+                  case "Jingle Ball $25":
+                  checker();
+                  break;
+
+                  case "Crinkle Toy $25":
+                  checker();
+                  break;
+                }
+        });
+}
+
+// to check request against stock
+function checker() {
+
+    // if ==! enough, log 'Insufficient Quantity In Stock'
+
+    // then, if === enough, fill order by updating stock left in db and displaying total cost of purchase
+
+
+
+}
---
+++ bamazonDB.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE bamazon;
+USE bamazon; 
+CREATE TABLE products(
+	item_id INT(10) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(30) NOT NULL,
+	price INT(10) NOT NULL,
+	stock_quantity INT(10) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES('Kitty Condo', 'pets', 125, 10),
+		('Catnip Bag - small', 'pets', 5, 10),
+		('Catnip Bag - large', 'pets', 10, 10),
+		('Catnip Mouse', 'pets', 10, 10),
+		('Cat Bed', 'pets', 40, 10),
+		('Litterbox', 'pets', 7, 10),
+		('Kitty Litter', 'pets', 35, 10),
+		('Cosmic Toy', 'pets', 5, 10),
+		('Jingle Ball', 'pets', 25, 10),
+		('Crinkle Toy', 'pets', 25, 10);
+        
+        
+SELECT * FROM products

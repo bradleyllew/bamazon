6491119d9a7e61f5ccca7753e93f5ea790e009c4
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-10-07 17:02:34 -0400

    added remainingStock to update inventory.Switched var to const/let.  Created array 'prices' to attempt to update total user cost at end of purchase process

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,16 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// requiring the programs needed
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var products = ["Kitty Condo $125", "Catnip Bag(sm) $5", "Catnip Bag(lg) $10", "Catnip Mouse $10", "Cat Bed $40", "Litterbox $7", "Kitty Litter $35", "Cosmic Toy $5", "Jingle Ball $25", "Crinkle Toy $25"];
-var howMany = 0;
+// setting up the variables
+const prices = [125, 5, 10, 10, 40, 7, 35, 5, 25, 25];
+// var for how many (stock_quantity user wants to buy)
+let howMany = 0;
+// var for user choice of product
+let choices = "";
+
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -18,13 +24,6 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-// connect to the mysql server and sql database
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("before the start function")
-//     // run the start function after the connection is made to prompt the user
-//     start();
-// });
 start();
 // to display all the products available to customer, including id, name, & price
 function start() {
@@ -47,7 +46,7 @@ function buy() {
             {
                 name: "username",
                 type: "input",
-                message: "What is your kitty's name?"
+                message: "Welcome to Bamazon! What is your kitty's name?"
             },
             {
                 name: "toBuy",
@@ -63,92 +62,56 @@ function buy() {
         .then(function (answer) {
             console.log(answer)
             howMany = answer.howMany;
+            choices = answer.toBuy;
+            checker();
             // based on their answer, welcome owner & kitty
             if (answer.username) {
                 console.log(answer.username + " is going to love their new " + answer.toBuy);
             }
             // based on their answer, check the stock quantities
-            switch (answer) {
-                case "Kitty Condo $125":
-                    checker();
-                    break;
+            function checker() {
+                let stock = 10;
+                let remainingStock = (stock - answer.howMany);
+                let totalPrice = ()
 
-                case "Catnip Bag(sm) $5":
-                    checker();
-                    break;
-
-                case "Catnip Bag(lg) $10":
-                    checker();
-                    break;
-
-                case "Catnip Mouse $10":
-                    checker();
-                    break;
-
-                case "Cat Bed $40":
-                    checker();
-                    break;
-
-                case "Litterbox $7":
-                    checker();
-                    break;
-
-                case "Kitty Litter $35":
-                    checker();
-                    break;
-
-                case "Cosmic Toy $5":
-                    checker();
-                    break;
-
-                case "Jingle Ball $25":
-                    checker();
-                    break;
-
-                case "Crinkle Toy $25":
-                    checker();
-                    break;
-            }
+                // if ==! enough, log 'Insufficient Quantity In Stock'
+                if (answer.howMany > 10) {
+                    console.log("Sorry, insufficient quantity! Try again?");
+                    // restart the app so user can pick again
+                    buy();
+                }
+                // if === enough , updates quantity in table and displays -------- needs to show total price
+                else if (remainingStock <= 10) {
+                    console.log("We have " + remainingStock + "_" + answer.toBuy + " left! Thanks for shopping Bamazon!");
+                    let purchase = function () {
+                        // update quantity
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: remainingStock.id
+                                },
+                                {
+                                    product_name: choices.id
+                                }
+                            ]
+                        );
+                        connection.query("SELECT item_id, product_name, stock_quantity FROM products", function (err, result) {
+                            if (err) throw err;
+                            console.log("RESULTS UPDATED:");
+                            console.log(result);
+                        });
+                        purchase();
+                    };
+                    buy();
+                };
+            };
         });
+
 }
 
-// to check request against stock
-function checker() {
-
-    // if ==! enough, log 'Insufficient Quantity In Stock'
-    if (howMany > 10) {
-        console.log("Sorry, insufficient quantity! Try again?");
-        // restart the app so user can pick again
-        start();
-    }
-    // if === enough , updates quantity in table and displays -------- needs to show total price
-    else if (howMany <= 10) {
-        var purchase = function () {
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: howMany.id
-                    },
-                    {
-                        product_name: choices.id
-                    }
-                ]
-            );
-            connection.query("SELECT item_id, product_name, price FROM products", function (err, result) {
-                if (err) throw err;
-                console.log(result);
-            });
-            // function(error) {
-            //     if (error) throw err;
-            //     console.log("Thanks for your purchase!");
-            //     start();
-            // }
-
-        }
-        purchase();
-    };
-
-
-
+function total () {
+// for value of price array === to item in choices array???
+    for (prices[x] === choices[x])
+    console.log("Your total today is " + (prices[x] * remainingStock));
 }
